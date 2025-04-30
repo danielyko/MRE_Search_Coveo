accb8bdeb4d8677313dc1999dbb0918d2566f314
Danielyko <danielyko@gmail.com>
2025-04-30 11:15:38 +0100

    add dotenv - hide API KEY and Node Modules

--- Headless_mre_searh/src/Engine.ts
+++ Headless_mre_searh/src/Engine.ts
@@ -4,6 +4,9 @@ import {
   SearchEngine,
 } from "@coveo/headless";
 
+//updated dotenv
+import dotenv from 'dotenv';
+dotenv.config();
 
 const FIELDS = [
   "ec_images",
@@ -14,11 +17,15 @@ const registerAdditionalFields = (headlessEngine: SearchEngine) => {
   headlessEngine.dispatch(fieldActions.registerFieldsToInclude(FIELDS));
   return headlessEngine;
 }
+// Check that required env vars are present
+if (!process.env.ACCESS_TOKEN) {
+  throw new Error("ACCESS_TOKEN is not defined in environment variables.");
+}
 
 const buildEngine = buildSearchEngine({
   configuration: {
     organizationId: "barcagroupproductionkwvdy6lp",
-    accessToken: "xx5a7943ef-ea52-42e5-8742-51198cc651f7"
+    accessToken: process.env.ACCESS_TOKEN as string,
   },
 });
 
